affd3256c38da7a54cd4e1b3fd5349aba1a9fd45
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2021-01-28 17:04:48 +0900

    GraphQL-Argument

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,14 @@
 import { Resolver, Query, Args } from "../../node_modules/@nestjs/graphql";
 import { Restaurant } from './entities/restaurant.entity';
 
-@Resolver()
+@Resolver(of => Restaurant)
+/* Resolver 데코레이터에 of=>Restaurant를 추가해줌으로써
+코드의 표현력이 더 좋아진다. */
 export class RestaurantsResolver {
-    @Query(returns => Restaurant)
-        myRestaurant() {
-        return true;
+    @Query(returns => [Restaurant]) /* GraphQL에서 대괄호 표현 방법 */
+        //veganOnly라는 이름을 가진 argument 요청하기
+        restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] /* typesrcript에서 대괄호 표현 방법 */ {
+        console.log(veganOnly); // true
+        return [];
     }
 }
