b483a874cc69c40ef14a93b43a880b59e1307e1b
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2021-02-03 06:42:33 +0900

    TYPEORM and NEST-Injecting The Repository

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsResolver } from './restaurants.resolver';
+import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantService } from './restaurants.service';
 
 @Module({
-    providers: [RestaurantsResolver],
+    imports: [TypeOrmModule.forFeature([Restaurant])],
+    providers: [RestaurantsResolver, RestaurantService],
 })
 export class RestaurantsModule {}
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,16 +1,17 @@
 import { Resolver, Query, Args, Mutation } from "../../node_modules/@nestjs/graphql";
 import { Restaurant } from './entities/restaurant.entity';
 import { CreateRestaurantDto } from "./dtos/create-restaurant.dto";
+import { RestaurantService } from './restaurants.service';
 
 @Resolver(of => Restaurant)
 /* Resolver 데코레이터에 of=>Restaurant를 추가해줌으로써
 코드의 표현력이 더 좋아진다. */
 export class RestaurantsResolver {
+    constructor(private readonly restaurantService: RestaurantService) {}
     @Query(returns => [Restaurant]) /* GraphQL에서 대괄호 표현 방법 */
         //veganOnly라는 이름을 가진 argument 요청하기
-        restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] /* typesrcript에서 대괄호 표현 방법 */ {
-        console.log(veganOnly); // true
-        return [];
+        restaurants(): Promise<Restaurant[]> {
+        return this.restaurantService.getAll();
     }
     @Mutation(returns => Boolean)
     createRestaurant(
