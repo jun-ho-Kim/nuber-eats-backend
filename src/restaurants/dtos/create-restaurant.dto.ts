4c214592cd479525c8026b7fa2926a65b961434a
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2021-02-03 16:15:13 +0900

    TYPEORM AND NEST-Mapped Types

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,22 +1,8 @@
-import { ArgsType, InputType, Field, Args } from '@nestjs/graphql';
+import { ArgsType, InputType, Field, Args, OmitType } from '@nestjs/graphql';
 import { IsBoolean, IsString, Length } from 'class-validator';
+import { Restaurant } from '../entities/restaurant.entity';
 
-@ArgsType()
-export class CreateRestaurantDto {
-    @Field(type => String)
-    @IsString()
-    @Length(5, 10)
-    name: string;
-
-    @Field(type => Boolean)
-    @IsBoolean()
-    isVegan:string;
-
-    @Field(type => String)
-    @IsString()
-    address: string;
-    
-    @Field(type => String)
-    @IsString()
-    ownersName: string;
+@InputType()
+export class CreateRestaurantDto 
+extends OmitType(Restaurant, ['id']) {
 }
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,6 +1,8 @@
-import {ObjectType, Field} from "@nestjs/graphql"
+import {ObjectType, Field, InputType} from "@nestjs/graphql"
 import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { IsBoolean, IsString, Length } from "../../../node_modules/class-validator";
 
+@InputType({isAbstract : true })
 @ObjectType() /* <- GraphQL를 위한 것  */
 @Entity() /* <- TypeORM를 위한 것  */
 export class Restaurant {
@@ -10,17 +12,22 @@ export class Restaurant {
 
     @Field(type => String) /* <- 이 타입은 GraphQL를 위한 것 */
     @Column() /* <- 이 타입은 TypeORM를 위한 것  */
+    @IsString()
+    @Length(5)
     name: string; /* <- 이 타입은 타입스크립트를 위한 것 */
 
     @Field(type => Boolean)
     @Column()
+    @IsBoolean()
     isVegan: Boolean;
 
     @Field(type => String)
     @Column()
+    @IsString()
     address: string;
 
     @Field(type => String)
     @Column()
+    @IsString()
     ownersName: string;
 }
