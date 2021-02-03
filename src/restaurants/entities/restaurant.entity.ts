4ebeaf75115a453295e654ae535a4f3dc9932118
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2021-02-03 17:00:41 +0900

    TYPEORM AND NEST-Optional Types and Columns

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,7 +1,8 @@
 import {ObjectType, Field, InputType} from "@nestjs/graphql"
 import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
-import { IsBoolean, IsString, Length } from "../../../node_modules/class-validator";
+import { IsBoolean, IsOptional, IsString, Length } from "../../../node_modules/class-validator";
 
+//entity 파일 안에 class validator에 의해 validate 되고 있다.
 @InputType({isAbstract : true })
 @ObjectType() /* <- GraphQL를 위한 것  */
 @Entity() /* <- TypeORM를 위한 것  */
@@ -16,8 +17,12 @@ export class Restaurant {
     @Length(5)
     name: string; /* <- 이 타입은 타입스크립트를 위한 것 */
 
-    @Field(type => Boolean)
-    @Column()
+    @Field(type => Boolean, {defaultValue: true}) 
+    //ㄴ graphql 스키마에서 이 필드의 defaultValue가 true라는 뜻
+    @Column({ default: true}) 
+    //ㄴ DB에서 이 필드의 defaultValue가 true 라는 뜻
+    @IsOptional() 
+    //ㄴ validation은 optional이고 만약 value가 있다면 boolean이어야 한다는 뜻(dto를 위한 것들)
     @IsBoolean()
     isVegan: Boolean;
 
