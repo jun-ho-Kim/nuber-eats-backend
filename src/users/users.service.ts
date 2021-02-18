af9ae75cfbaab51e1a9d697a1261641e7664412a
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2021-02-18 16:32:02 +0900

    userProfile: update method -> JS syntex

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -72,7 +72,14 @@ export class UserService {
     async findById(id: number): Promise<User> {
         return this.users.findOne({id});
     }
-    async editProfile(userId: number, editProfileInput: EditProfileInput) {
-        return this.users.update(userId, {...editProfileInput});
+    async editProfile(userId: number, {email, password}: EditProfileInput) {
+        const user = await this.users.findOne(userId);
+        if(email) {
+           user.email = email 
+        }
+        if(password) {
+            user.password = password
+        }
+        return this.users.save(user);
     }
 }
