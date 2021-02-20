40b24b361c0b1b6a72ddf4ba16fad19f017cada0
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2021-02-20 19:13:49 +0900

    User: Cleaning the Code

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -19,95 +19,39 @@ export class UserResolver {
     @Mutation(returns => CreateAccountOutput)
     async createAccount(@Args('input') createAccountInput: CreateAccountInput): 
     Promise<CreateAccountOutput>  {
-        try {
-            return this.usersService.createAccount(createAccountInput);
-            // const {ok, error} = await this.usersService.createAccount(createAccountInput);
-            //     return {
-            //         ok,
-            //         error
-            //     }
-        } catch(error) {
-            return {
-                error,
-                ok:false
-            };
-        }
+        return this.usersService.createAccount(createAccountInput);
     }
+
     @Mutation(returns => LoginOutput)
     async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
-        try {
-            return this.usersService.login(loginInput);
-        } catch (error) {
-            return {
-                ok: false,
-                error
-            }
-        }
+        return this.usersService.login(loginInput);
     }
+
     @Query(returns => User)
     @UseGuards(AuthGuard)
     me(@AuthUser() authUser: User) {
         return authUser
     }
-    // me(@Context() context) {
-    //     if(!context.user){
-    //         return false
-    //     } else {
-    //         return context.user;
-    //     }
-    // }
+
     @UseGuards(AuthGuard)
     @Query(returns => UserProfileOutput)
     async userProfile(
         @Args() userProfileInput: UserProfileInput
     ): Promise<UserProfileOutput> {
-        try {
-            const user = await this.usersService.findById(userProfileInput.userId);
-            if(!user) {
-                throw Error()
-            }
-            return {
-                ok: true,
-                user
-            }
-        } catch(e) {
-            return {
-                error: "User Not Found",
-                ok: false
-            }
-
-        }
+        return this.usersService.findById(userProfileInput.userId);
     }
+
     @UseGuards(AuthGuard)
     @Mutation(returns => EditProfileOutput)
     async editProfile(
         @AuthUser() authUser: User,
         @Args('input') editProfileInput: EditProfileInput
     ): Promise<EditProfileOutput> {
-        try {
-            await this.usersService.editProfile(authUser.id, editProfileInput);
-            return {
-                ok: true
-            }
-        } catch(error) {
-            return {
-                ok: false,
-                error,
-            }
-        }
+        return this.usersService.editProfile(authUser.id, editProfileInput);
     }
+    
     @Mutation(returns => VerifyEmailOutput)
-    async VerifyEmail(@Args('input') {code}: VerifyEmailInput):Promise<VerifyEmailOutput> {
-        try {
-            await this.usersService.verifyEmail(code);
-            return {
-                ok: true
-            };
-        } catch(error) {
-            return {
-                error,
-                ok: false
-            };
-        }
+    VerifyEmail(@Args('input') {code}: VerifyEmailInput):Promise<VerifyEmailOutput> {
+        return this.usersService.verifyEmail(code);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,14 +2,13 @@ import { Injectable } from "@nestjs/common";
 import { User } from "./entities/user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
-import * as jwt from "jsonwebtoken"
-import { CreateAccountInput } from "./dtos/create-account.dto";
-import { LoginInput} from "./dtos/login.dto";
-import { ConfigService } from "../../node_modules/@nestjs/config";
+import { CreateAccountInput, CreateAccountOutput } from "./dtos/create-account.dto";
+import { LoginInput, LoginOutput} from "./dtos/login.dto";
 import { JwtService } from "../jwt/jwt.service";
-import { EditProfileInput } from "./dtos/edit-profile.dto";
+import { EditProfileInput, EditProfileOutput } from "./dtos/edit-profile.dto";
 import { Verification } from "./entities/verification.entity";
-import { VerifyEmailInput } from "./dtos/verify-email.dto";
+import { VerifyEmailInput, VerifyEmailOutput } from "./dtos/verify-email.dto";
+import { UserProfileOutput } from "./dtos/user-profile.dto";
 
 
 @Injectable () 
@@ -25,7 +24,7 @@ export class UserService {
     }
 
     async createAccount({email, password, role
-    }: CreateAccountInput): Promise<{ok:boolean, error?:string}> {
+    }: CreateAccountInput): Promise<CreateAccountOutput> {
         try {
             const exist = await this.users.findOne({email});
             if(exist) {
@@ -49,7 +48,7 @@ export class UserService {
     async login({
         email, 
         password}
-        : LoginInput): Promise<{ok: boolean, error?: string, token?: string}> {
+        : LoginInput): Promise<LoginOutput> {
         try {
             const user = await this.users.findOne(
                 {email},
@@ -80,22 +79,48 @@ export class UserService {
             };
         }
     }
-    async findById(id: number): Promise<User> {
-        return this.users.findOne({id});
-    }
-    async editProfile(userId: number, {email, password}: EditProfileInput) {
-        const user = await this.users.findOne(userId);
-        if(email) {
-           user.email = email;
-           user.verified = false;
-           await this.verifications.save(this.verifications.create({user}));
+    async findById(id: number): Promise<UserProfileOutput> {
+        try {
+            const user = await this.users.findOne({id});
+            if(user) {
+                return {
+                    ok: true,
+                    user
+                }
+            }
+        } catch(error) {
+            return {
+                error,
+                ok: false
+            }
+
         }
-        if(password) {
-            user.password = password
-        }
-        return this.users.save(user);
+        
     }
-    async verifyEmail(code: string): Promise<boolean> {
+    async editProfile(userId: number, {email, password}: EditProfileInput
+    ): Promise<EditProfileOutput> {
+        try {
+            const user = await this.users.findOne(userId);
+            if(email) {
+               user.email = email;
+               user.verified = false;
+               await this.verifications.save(this.verifications.create({user}));
+            }
+            if(password) {
+                user.password = password
+            }
+            await this.users.save(user);
+            return {
+                ok: true,
+            }
+        } catch(error) {
+            return {
+                ok: false,
+                error: 'Could not update profile'
+            }
+        }
+    }
+    async verifyEmail(code: string): Promise<VerifyEmailOutput> {
         const verification = await this.verifications.findOne(
             {code},
             {relations: ['user']})
@@ -103,12 +128,11 @@ export class UserService {
             if(verification) {
                 verification.user.verified = true;
                 this.users.save(verification.user);
-                return true;
             }
-            throw new Error()
-        } catch(e) {
-            console.log(e);
-            return false;
+            return {ok: true};
+            return { ok: false, error: 'Verification not found.' };
+        } catch(error) {
+            return {ok: false, error};
         }
     } 
 }
