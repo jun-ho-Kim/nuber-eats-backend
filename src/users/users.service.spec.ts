db0f3989f3e3ec2aca6d41303204650c797107ba
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2021-02-23 19:52:25 +0900

    UNIT TESTING-USER SERVICE: createAccount Test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -10,14 +10,16 @@ import { Repository } from "../../node_modules/typeorm";
 type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 describe('UserService', () => {
-    let service: UserService;
+    let service: UserService;Verification
     let usersRepository: MockRepository<User>;
+    let verificationRepository: MockRepository<Verification>;
+    let mailService: MailService;
 
-    const mockRepository = {
+    const mockRepository = () => ({
         findOne: jest.fn(),
         save: jest.fn(),
         create: jest.fn(),
-    };
+    });
 
     const mockJwtRepository = {
         sign: jest.fn(),
@@ -34,11 +36,11 @@ describe('UserService', () => {
                 UserService,
                 {
                     provide: getRepositoryToken(User),
-                    useValue: mockRepository
+                    useValue: mockRepository(),
                 },
                 {
                     provide: getRepositoryToken(Verification),
-                    useValue: mockRepository
+                    useValue: mockRepository(),
                 },
                 {
                     provide: JwtService,
@@ -52,29 +54,63 @@ describe('UserService', () => {
         }).compile();
         service = module.get<UserService>(UserService);
         usersRepository = module.get(getRepositoryToken(User));
+        verificationRepository = module.get(getRepositoryToken(Verification));
+        mailService = module.get<MailService>(MailService);
     });
 
     it('should be defined', () => {
         expect(service).toBeDefined();
     });
-
+    
     describe('createAccount', () => {
+        const createAccountArgs = {
+            email: '',
+            password: '',
+            role: 0,
+        };
         it('should fail if user exists', async () => {
+            //Promise의 ResolverdValue를 속일 것이다.
+            //jest가 중간에 fineOne 함수를 가로채고 반환 값을 속일 것이다.
             usersRepository.findOne.mockResolvedValue({
-                id: 1,
+                id: '1',
                 email: '',
             });
-            const result = await service.createAccount({
-                email: '2',
-                password: '',
-                role: 0,
-            });
+            const result = await service.createAccount(createAccountArgs);
             expect(result).toMatchObject({
                 ok: false,
                 error: 'There is a user with that email already',
             });
         });
-    });
+            it('should create a new User', async () => {
+                usersRepository.findOne.mockResolvedValue(undefined);
+                usersRepository.create.mockReturnValue(createAccountArgs);
+                usersRepository.save.mockResolvedValue(createAccountArgs);
+                verificationRepository.create.mockReturnValue(createAccountArgs);
+                verificationRepository.save.mockResolvedValue({
+                    code: 'code',
+                });
+
+                const result = await service.createAccount(createAccountArgs);
+                expect(usersRepository.create).toHaveBeenCalledTimes(1);
+                expect(usersRepository.create).toHaveBeenCalledWith(createAccountArgs);
+
+                expect(usersRepository.save).toHaveBeenCalledTimes(1);
+                expect(usersRepository.save).toHaveBeenCalledWith(createAccountArgs);
+
+                expect(verificationRepository.create).toHaveBeenCalledTimes(1);
+                expect(verificationRepository.create).toHaveBeenCalledWith({
+                    user: createAccountArgs
+                });
+                
+                expect(mailService.sendVerificationEmail).toHaveBeenCalledTimes(1);
+                expect(mailService.sendVerificationEmail).toHaveBeenCalledWith(
+                    expect.any(String),
+                    expect.any(String),
+                );
+
+                expect(result).toEqual({ok :true});
+                });
+            });
 
     it.todo('createAccount');
     it.todo('login');
