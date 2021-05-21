467ddada0574d1977d1ad214994070a83d5b12cf
jun-ho-Kim <wnsgh5049@naver.com>
2021-05-21 13:52:48 +0900

    DEPLOY: Environment, Port

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,16 +26,16 @@ import { UploadsModule } from './uploads/uploads.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test", 
-      ignoreEnvFile: process.env.NODE_ENV === "prod",
+      ignoreEnvFile: process.env.NODE_ENV === "production",
       validationSchema: Joi.object({
         NODE_ENV: Joi.string()
-          .valid('dev', 'prod')
+          .valid('dev', 'production', 'test')
           .required(),
-          DB_HOST: Joi.string().required(),
-          DB_PORT: Joi.string().required(),
-          DB_USERNAME: Joi.string().required(),
-          DB_PASSWORD: Joi.string().required(),
-          DB_NAME: Joi.string().required(),
+          DB_HOST: Joi.string(),
+          DB_PORT: Joi.string(),
+          DB_USERNAME: Joi.string(),
+          DB_PASSWORD: Joi.string(),
+          DB_NAME: Joi.string(),
           PRIVATE_KEY: Joi.string().required(),
           MAILGUN_API_KEY: Joi.string().required(),
           MAILGUN_DOMAIN_NAME: Joi.string().required(),
@@ -54,11 +54,15 @@ import { UploadsModule } from './uploads/uploads.module';
   }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      ...(process.env.DATABASE_URL
+        ?{url: process.env.DATABASE_URL}
+        :{
+          host: process.env.DB_HOST,
+          port: +process.env.DB_PORT,
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+        }),
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: process.env.NODE_ENV !== 'prod',
       entities: [User, Verification, Category, Restaurant, Dish, Order, OrderItem],
