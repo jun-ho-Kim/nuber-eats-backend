5eeeab1b8123ea188e0c61ead431f90dc46cbbf9
jun-ho-Kim <wnsgh5049@naver.com>
2021-05-21 15:38:35 +0900

    DEPLOY: change app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,71 +1,81 @@
-import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import * as Joi from 'joi';
-import { GraphQLModule} from "@nestjs/graphql";
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
-import { CommonModule } from './common/common.module';
 import { User } from './users/entities/user.entity';
 import { JwtModule } from './jwt/jwt.module';
 import { AuthModule } from './auth/auth.module';
 import { Verification } from './users/entities/verification.entity';
 import { MailModule } from './mail/mail.module';
-import { Category } from './restaurants/entities/category.entity';
 import { Restaurant } from './restaurants/entities/restaurant.entity';
+import { Category } from './restaurants/entities/category.entity';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { Dish } from './restaurants/entities/dish.entity';
 import { OrdersModule } from './orders/orders.module';
 import { Order } from './orders/entities/order.entity';
 import { OrderItem } from './orders/entities/order-items.entity';
+import { CommonModule } from './common/common.module';
 import { UploadsModule } from './uploads/uploads.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test", 
-      ignoreEnvFile: process.env.NODE_ENV === "production",
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string()
           .valid('dev', 'production', 'test')
           .required(),
-          DB_HOST: Joi.string(),
-          DB_PORT: Joi.string(),
-          DB_USERNAME: Joi.string(),
-          DB_PASSWORD: Joi.string(),
-          DB_NAME: Joi.string(),
-          PRIVATE_KEY: Joi.string(),
-          MAILGUN_API_KEY: Joi.string(),
-          MAILGUN_DOMAIN_NAME: Joi.string(),
-          MAILGUN_FROM_EMAIL: Joi.string(),
-          AWS_S3_ACCESSKEYID: Joi.string(),
-          AWS_S3_SECRETACCESSKEY: Joi.string(),
+        DB_HOST: Joi.string(),
+        DB_PORT: Joi.string(),
+        DB_USERNAME: Joi.string(),
+        DB_PASSWORD: Joi.string(),
+        DB_NAME: Joi.string(),
+        PRIVATE_KEY: Joi.string().required(),
+        MAILGUN_API_KEY: Joi.string().required(),
+        MAILGUN_DOMAIN_NAME: Joi.string().required(),
+        MAILGUN_FROM_EMAIL: Joi.string().required(),
+        AAWS_S3_ACCESSKEYID: Joi.string().required(),
+        AWS_S3_SECRETACCESSKEY: Joi.string().required(),
       }),
     }),
-    GraphQLModule.forRoot({
-      installSubscriptionHandlers: true,
-      autoSchemaFile: true,
-      context: ({ req, connection }) => { 
-        const TOKEN_KEY = 'x-jwt';
-        return {
-          token: req ? req.headers[TOKEN_KEY] : connection.context[TOKEN_KEY]
-        }
-    }
-  }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       ...(process.env.DATABASE_URL
-        ?{url: process.env.DATABASE_URL}
-        :{
-          host: process.env.DB_HOST,
-          port: +process.env.DB_PORT,
-          username: process.env.DB_USERNAME,
-          password: process.env.DB_PASSWORD,
-          database: process.env.DB_NAME,
-        }),
-      synchronize: process.env.NODE_ENV !== 'production',
-      logging: process.env.NODE_ENV !== 'production',
-      entities: [User, Verification, Category, Restaurant, Dish, Order, OrderItem],
+        ? { url: process.env.DATABASE_URL }
+        : {
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+          }),
+      synchronize: process.env.NODE_ENV !== 'prod',
+      logging:
+        process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
+      entities: [
+        User,
+        Verification,
+        Restaurant,
+        Category,
+        Dish,
+        Order,
+        OrderItem,
+      ],
+    }),
+    GraphQLModule.forRoot({
+      playground: process.env.NODE_ENV !== 'production',
+      installSubscriptionHandlers: true,
+      autoSchemaFile: true,
+      context: ({ req, connection }) => {
+        const TOKEN_KEY = 'x-jwt';
+        return {
+          token: req ? req.headers[TOKEN_KEY] : connection.context[TOKEN_KEY],
+        };
+      },
     }),
     JwtModule.forRoot({
       privateKey: process.env.PRIVATE_KEY,
@@ -78,11 +88,11 @@ import { UploadsModule } from './uploads/uploads.module';
     AuthModule,
     UsersModule,
     RestaurantsModule,
-    CommonModule,
     OrdersModule,
+    CommonModule,
     UploadsModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {};
+export class AppModule {}
